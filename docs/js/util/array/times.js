90eb8ddde30ef7dd42dd105655415dcfd34315d1
ytyaru <yttry0@gmail.com>
2024-02-11 11:26:55 +0900

    回数バグ修正

--- docs/js/util/array/times.js
+++ docs/js/util/array/times.js
@@ -1,4 +1,4 @@
 Array.times = function(num, fn, params) { // num回数だけfnを実行する
-    if (params) { return [...Array(5)].map((_,i)=>fn(i, ...params)) }
-    else { return [...Array(5)].map((_,i)=>fn(i)) }
+    if (params) { return [...Array(num)].map((_,i)=>fn(i, ...params)) }
+    else { return [...Array(num)].map((_,i)=>fn(i)) }
 }
