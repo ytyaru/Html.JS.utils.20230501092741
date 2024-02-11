c62ca3fff3bb799426cef0538ae473490c01593e
ytyaru <yttry0@gmail.com>
2024-02-11 11:25:42 +0900

    不要なconsole.logを消した

--- docs/js/util/array/times.js
+++ docs/js/util/array/times.js
@@ -1,7 +1,4 @@
-//Array.prototype.times = function(num, fn, params) { // num回数だけfnを実行する
 Array.times = function(num, fn, params) { // num回数だけfnを実行する
     if (params) { return [...Array(5)].map((_,i)=>fn(i, ...params)) }
     else { return [...Array(5)].map((_,i)=>fn(i)) }
 }
-console.log(Array.times)
-console.log(Array.prototype.times)
