a91282f43c5ee72ceeecdf2a6710eee4e6e4671c
ytyaru <yttry0@gmail.com>
2025-11-05 20:58:55 +0900

    mod digital-clock.js

--- docs/js/util/web-component/digital-clock.backup.js
+++ docs/js/util/web-component/digital-clock.backup.js
@@ -2,39 +2,29 @@
 class DigitalClock extends HTMLElement {// 全画面時のみ時分HH:MMを表示する。縦書き時縦中横にする。
     constructor() {
         super();
-        this._ = {timer:null, attrs:{hidden:{type:'boolean', value:false}}, listened:false};
+        this._ = {timer:null, attrs:{hidden:{type:'boolean', value:false}}};
     }
     static get observedAttributes() {return ['hidden'];}
     connectedCallback() {
-        if (!this.querySelector('[name="hours"]')) {
-            this.style.lineHeight = '1em';
-            this.style.letterSpacing = '0em';
-            /*
-            this.append(
-                Dom.tags.span({name:'hours', style:'writing-mode:vertical-rl;text-combine-upright:all;'}, '--'), 
-                Dom.tags.span({name:'colon', style:'text-orientation:mixed;'}, ':'), 
-                Dom.tags.span({name:'minutes', style:'writing-mode:vertical-rl;text-combine-upright:all;'}, '--'), 
-            );
-            */
-            const N = 'span';
-            const H = document.createElement(N);
-            const C = document.createElement(N);
-            const M = document.createElement(N);
-            H.setAttribute('name', 'hours');
-            C.setAttribute('name', 'colon');
-            M.setAttribute('name', 'minutes');
-            H.style.writingMode = 'vertical-rl';
-            H.style.textOrientation = 'upright';
-            H.style.textCombineUpright = 'all';
-            C.style.textOrientation = 'mixed';
-            M.style.writingMode = 'vertical-rl';
-            M.style.textOrientation = 'upright';
-            M.style.textCombineUpright = 'all';
-            H.textContent = '--';
-            C.textContent = ':';
-            M.textContent = '--';
-            this.append(H, C, M);
-        }
+        this.#updateDisplay();
+        const N = 'span';
+        const H = document.createElement(N);
+        const C = document.createElement(N);
+        const M = document.createElement(N);
+        H.setAttribute('name', 'hours');
+        C.setAttribute('name', 'colon');
+        M.setAttribute('name', 'minutes');
+        H.style.writingMode = 'vertical-rl';
+        H.style.textOrientation = 'upright';
+        H.style.textCombineUpright = 'all';
+        C.style.textOrientation = 'mixed';
+        M.style.writingMode = 'vertical-rl';
+        M.style.textOrientation = 'upright';
+        M.style.textCombineUpright = 'all';
+        H.textContent = '--';
+        C.textContent = ':';
+        M.textContent = '--';
+        this.append(H, C, M);
         this.#createTimer();
         this.#addListener();
         this.#setNowTime();
@@ -77,26 +67,21 @@ class DigitalClock extends HTMLElement {// 全画面時のみ時分HH:MMを表�
         H.textContent = h; M.textContent = m;
         this.style.contentVisibility = 'auto';
     }
-    get #isFullScreen() {return window.screen.height===window.innerHeight;}
-    #updateDisplay() { this.style.display = !this.hidden && this.#isFullScreen ? 'inline' : 'none'; }
+    #updateDisplay() { this.style.contentVisibility = `${this._.attrs.hidden.value ? 'hidden' : 'auto'}`; }
     #createTimer() {if (!this._.timer && !this._.attrs.hidden.value) {this._.timer = setInterval(this.#setNowTime.bind(this), 1000);}}
     #removeTimer() {if (this._.timer) {clearInterval(this._.timer); this._.timer=null;}}
     #onFocusIn(e) {this.#setNowTime(); this.#createTimer();}
     #onFocusOut(e) {this.#removeTimer();}
-    #onResize(e) {this.#updateDisplay();}
+    #onResize(e) {this.hidden = !(window.screen.height===window.innerHeight);}
     #addListener() {// フォーカスの取得・喪失に合わせてタイマーON/OFF切替。focus/blurだとバブリングして複数回呼び出される。visibilitychangeだと他窓遷移時発火せず。
-        if (this._.listened) {return}
         window.addEventListener('focusin', this.#onFocusIn.bind(this));// 他のウインドウに遷移したときは発火する。最小化したりタブ遷移した時も！
         window.addEventListener('focusout', this.#onFocusOut.bind(this));// 他のウインドウに遷移したときは発火する。最小化したりタブ遷移した時も！
         window.addEventListener('resize', this.#onResize.bind(this));
-        this._.listened = true;
     }
     #removeListener() {
-        if (!this._.listened) {return}
         window.removeEventListener('focusin', this.#onFocusIn.bind(this));// 他のウインドウに遷移したときは発火する。最小化したりタブ遷移した時も！
         window.removeEventListener('focusout', this.#onFocusOut.bind(this));// 他のウインドウに遷移したときは発火する。最小化したりタブ遷移した時も！
         window.removeEventListener('resize', this.#onResize.bind(this));
-        this._.listened = false;
     }
 }
 customElements.define('digital-clock', DigitalClock); // <digital-clock></digital-clock>
